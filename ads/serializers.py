cd6242666080ad2068ea6b256dad25dab0702f7c
SChuprinenko <skillova@yandex.ru>
2024-09-04 07:00:45 +0300

    Написан контроллер создания объявлений, для авторизированных пользователей.

---
+++ ads/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Ad, Comment
+
+
+class AdSerializer(serializers.ModelSerializer):
+    """
+    Базовый сериализатор для модели объявления
+    """
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Базовый сериализатор для модели отзыва
+    """
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class AdDetailSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для детального просмотра одного объявления
+    """
+
+    comment = CommentSerializer(
+        source='comment_ad',
+        read_only=True,
+        many=True
+    )
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Ad
+from .serializers import AdDetailSerializer
+
+
+class AdCreateAPIView(generics.CreateAPIView):
+    """
+    Контроллер создания объявлений.
+    """
+    serializer_class = AdDetailSerializer
+    queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        Метод для автоматической привязки объявления к создателю.
+        """
+        serializer.save(author=self.request.user)
