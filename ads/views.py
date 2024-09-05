4538fbde01f5c32367f513a7cbcbdb81090a5641
SChuprinenko <skillova@yandex.ru>
2024-09-05 21:29:27 +0300

    ad/views: добавлены методы отношений в контроллерах (связи)

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -13,6 +14,14 @@ class AdCreateAPIView(generics.CreateAPIView):
     """
     serializer_class = AdDetailSerializer
     queryset = Ad.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Отношение объявления к автору
+        """
+        ad = serializer.save()
+        ad.author = self.request.user
+        ad.save()
 
 
 class AdListAPIView(generics.ListAPIView):
@@ -56,6 +65,14 @@ class MyAdListAPIView(generics.ListAPIView):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
 
+    def get_queryset(self):
+        """
+        Список объявлений автора
+        """
+        user = self.request.user
+        queryset = Ad.objects.filter(author=user)
+        return queryset
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
@@ -63,3 +80,22 @@ class CommentViewSet(viewsets.ModelViewSet):
     """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        """
+        Метод привязки отзыва к автору и объявлению
+        """
+        comment = serializer.save()
+        comment.author = self.request.user
+        comment.ad = Ad.objects.get(pk=self.kwargs["ad_pk"])
+        comment.save()
+
+    def get_queryset(self):
+        """
+        Метод для получения отзывов объявления
+        """
+        ad_pk = self.kwargs.get("ad_pk")
+        ad = get_object_or_404(Ad, id=ad_pk)
+        comment_list = ad.comment_ad.all()
+        return comment_list
+
