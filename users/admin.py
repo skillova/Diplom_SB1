26754c3e430cf14b1aaac6d5aff223c4f8e73f98
SChuprinenko <skillova@yandex.ru>
2024-09-10 20:31:54 +0300

    удалены поля в отображении в админ панели password(hach) и email

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,7 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'email', 'password', 'first_name', 'last_name', 'phone_number', 'email', 'role', 'avatar', 'is_active',
+        'id', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'is_active',
         'is_staff'
     )
     list_display_links = (
