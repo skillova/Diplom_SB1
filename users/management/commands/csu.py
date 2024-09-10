6ad67caba90d211639fa020f9f7b6048a9c1be1e
SChuprinenko <skillova@yandex.ru>
2024-09-10 20:29:40 +0300

    переменные окружения .env(_example) для CSU

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,6 +1,9 @@
 from django.core.management import BaseCommand
-
 from users.models import User
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class Command(BaseCommand):
@@ -9,13 +12,12 @@ class Command(BaseCommand):
     """
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@admin.admin',
-            first_name='fn_admin',
-            last_name='ln_admin',
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-            role='admin',
+            email=os.getenv('CSU_EMAIL'),
+            is_staff=os.getenv('CSU_IS_STAFF'),
+            is_superuser=os.getenv('CSU_IS_SUPERUSER'),
+            is_active=os.getenv('CSU_IS_ACTIVE'),
+            role=os.getenv('CSU_ROLE'),
         )
-        user.set_password('admin')
+        password = os.getenv('CSU_PASSWORD')
+        user.set_password(password)
         user.save()
