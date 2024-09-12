04d4561f21de78964da708bbac45cedb20558078
SChuprinenko <skillova@yandex.ru>
2024-09-12 05:59:23 +0300

    flake8

--- users/models.py
+++ users/models.py
@@ -1,8 +1,5 @@
-from django.contrib.auth.hashers import make_password
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# from .managers import UserManager
 
 NULLABLE = {
     'null': True,
@@ -88,11 +85,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.email} - {self.role}'
-
-    # objects = UserManager()
-    #
-    # def save(self, *args, **kwargs):
-    #     if 'pbkdf2_sha256' not in self.password:
-    #         password = make_password(self.password)
-    #         self.password = password
-    #     super().save(*args, **kwargs)
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
